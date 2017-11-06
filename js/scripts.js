9e749700af92b9e60e2b99af42e6f6fbd8193833
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2017-11-06 13:46:25 +0300

    add new prototype called roll

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 //business log
+//constructer holds player propeties
 function Player(playerName, turnScore, totalScore) {
   this.playerName = playerName;
   this.turnScore = turnScore;
   this.totalScore = totalScore;
+}
+Player.prototype.roll = function() {
+  var diceValues = [1, 2, 3, 4, 5, 6];
+  var rollValue = diceValues[Math.floor(Math.random() * diceValues.length)];
+  if (rollValue === 1) {
+    this.turnScore = 0;
+  } else {
+    this.turnScore = this.turnScore + rollValue;
+  };
+  return rollValue;
 }

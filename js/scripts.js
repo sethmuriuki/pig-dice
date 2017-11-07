836de13f0e898d3fb4f30404b8652b7844ee35da
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2017-11-07 10:38:54 +0300

    add some changes

--- js/scripts.js
+++ js/scripts.js
@@ -54,13 +54,39 @@ $("#btn-hold"), click(function(event) {
   $(".player1-roll-number").text("");
   $(".player1-turn-score").text(player1.turnScore);
   if (player1.totalScore >= 100) {
-    $("#game-section").fadeOut(400);
     $(".player1-victory").show().fadeOut(1200);
   } else {
 
     $(".player2-scored1").hide();
     $("#player2").show(900);
     $("#player1").hide(900);
-    $(".hold").show().fadeOut(1500);
   }
 });
+
+$(".player2-total-score").html("<span class='player2-total-score'>" + player2.totalScore + "</span>");
+
+ $("#btn-roll1").click(function(event) {
+   event.preventDefault();
+   var player2RolledNumber = player2.roll();
+   if (player2RolledNumber === 1) {
+     $(".player2-scored1").show().fadeOut(1000);
+     $("#well-play2").fadeOut(1000);
+     $("#well-play1").show(900);
+   }
+   $(".player2-rolled-number").text(player2RolledNumber);
+   $(".player2-turn-score").text(player2.turnScore);
+ });
+ $("#btn-hold1").click(function(event) {
+    event.preventDefault();
+    player2.score();
+    $(".player2-total-score").text(player2.totalScore);
+    $(".player2-roll-number").text("");
+    $(".player2-turn-score").text("");
+    if (player2.totalScore >= 100) {
+      $(".player2-victory").show().fadeOut(1200);
+    } else {
+    $("#well-play1").show(900);
+    $("#well-play2").hide(900);
+
+    }
+  });
